560871b896e7de8e2a1d6f60ce844fb8d8366cf1
Akash <shoaibsarkarakash019@gmail.com>
2026-02-15 01:44:34 +0300

    add tailwind with vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Nabver from './Component/Nerver/Nabver'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-center text-4xl font-bold mt-6'>Vite + React</h1>
+      <Nabver></Nabver>
+      
+    </>
+  )
+}
+
+export default App
